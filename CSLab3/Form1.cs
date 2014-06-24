cf96eb189e71cfd434e47ba02ce5221d462090eb
wjasiczek <wo.jasiczek@gmail.com>
2014-06-24 18:04:50 +0200

    first commit

---
+++ CSLab3/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Xml;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSLab3
+{
+    public partial class Form1 : Form
+    {
+        private String path;
+        private List<String> solutions;
+        private List<String> sources;
+        private String projectName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBoxPath.Text = folderBrowserDialog1.SelectedPath;
+                buttonLoad.Enabled = true;
+            }
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            textBoxConsole.Visible = true;
+            Height = 425;
+            path = textBoxPath.Text;
+            solutions = new List<String>();
+            sources = new List<String>();
+            Directory.CreateDirectory(path + "\\Sources");
+
+            textBoxConsole.Text += "Copying:" + "\r\n";
+
+            foreach (var file in Directory.GetFiles(path))
+            {
+                if (Path.GetExtension(file) == ".sln")
+                {
+                    solutions.Add(file);
+                    textBoxConsole.Text += Path.GetFileName(file) + "\r\n";
+                    File.Copy(path + "\\" + Path.GetFileName(file), path + "\\Sources\\" + Path.GetFileName(file), true);
+                }
+            }
+
+            char[] trimChar = { '\"', ',' };
+
+            foreach (var sln in solutions)
+            {
+                String[] fileTextLines = File.ReadAllLines(sln);
+                List<String[]> shreddedString = new List<String[]>();
+
+                foreach (var line in fileTextLines)
+                {
+                    if (line.Contains("Project(\""))
+                    {
+                        shreddedString.Add(line.Split(' '));
+                    }
+                }
+
+                projectName = shreddedString[0][2].Trim(trimChar);
+
+                foreach (var proj in shreddedString)
+                {
+                    File.Copy(path + "\\" + proj[3].Trim(trimChar), path + "\\Sources\\" + Path.GetFileName(proj[3].Trim(trimChar)), true);
+                    textBoxConsole.Text += proj[3].Trim(trimChar) + "\r\n";
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(path + "\\" + proj[3].Trim(trimChar));
+                    var nsmgr = new XmlNamespaceManager(doc.NameTable);
+                    nsmgr.AddNamespace("Project", doc.DocumentElement.NamespaceURI);
+                    XmlNodeList itemCompileNodes = doc.SelectNodes("//Project:Compile", nsmgr);
+
+                    foreach (XmlNode node in itemCompileNodes)
+                    {
+                        sources.Add(node.Attributes["Include"].Value);
+                        textBoxConsole.Text += node.Attributes["Include"].Value + "\r\n";
+                    }
+
+                    /*XmlNodeList noneNodes = doc.SelectNodes("//Project:None", nsmgr);
+                    textBoxConsole.Text += noneNodes[1].Attributes["Include"].Value + "\r\n";
+                    sources.Add(noneNodes[1].Attributes["Include"].Value);*/
+                }
+
+                foreach (var file in sources)
+                {
+                    File.Copy(path + "\\" + projectName + "\\" + file, path + "\\Sources\\" + Path.GetFileName(file), true);
+                }
+
+                textBoxConsole.Text += "\r\n";
+            }
+
+            ZipFile.CreateFromDirectory(path + "\\Sources", path + "\\Sources.zip");
+        }
+    }
+}
---
+++ Sources/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace CSLab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.textBoxConsole = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(14, 63);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(299, 22);
+            this.textBoxPath.TabIndex = 0;
+            // 
+            // labelPath
+            // 
+            this.labelPath.AutoSize = true;
+            this.labelPath.Location = new System.Drawing.Point(12, 43);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(57, 17);
+            this.labelPath.TabIndex = 1;
+            this.labelPath.Text = "Sciezka";
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(319, 62);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(75, 23);
+            this.buttonEdit.TabIndex = 2;
+            this.buttonEdit.Text = "Edytuj";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Enabled = false;
+            this.buttonLoad.Location = new System.Drawing.Point(15, 102);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 3;
+            this.buttonLoad.Text = "Wczytaj";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // textBoxConsole
+            // 
+            this.textBoxConsole.Location = new System.Drawing.Point(11, 172);
+            this.textBoxConsole.Multiline = true;
+            this.textBoxConsole.Name = "textBoxConsole";
+            this.textBoxConsole.ReadOnly = true;
+            this.textBoxConsole.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxConsole.Size = new System.Drawing.Size(379, 196);
+            this.textBoxConsole.TabIndex = 4;
+            this.textBoxConsole.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 145);
+            this.Controls.Add(this.textBoxConsole);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.labelPath);
+            this.Controls.Add(this.textBoxPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.TextBox textBoxConsole;
+    }
+}
+
